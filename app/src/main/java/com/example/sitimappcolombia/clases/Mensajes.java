2faef89fa1af0aecc4b3b7551da08ffc84049152
Juan Esteban Ortegon <juanortegon0119@outlook.com>
2021-11-11 01:34:56 -0500

    intents y mensajes Juan

--- app/src/main/java/com/example/sitimappcolombia/clases/Mensajes.java
+++ app/src/main/java/com/example/sitimappcolombia/clases/Mensajes.java
@@ -2,6 +2,8 @@ package com.example.sitimappcolombia.clases;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.icu.util.LocaleData;
 
 public class Mensajes {
     private Context contexto;
@@ -15,6 +17,20 @@ public class Mensajes {
         AlertDialog.Builder msj = new AlertDialog.Builder(this.contexto);
         msj.setTitle(titulo);
         msj.setMessage(cuerpo);
+        msj.create();
+        msj.show();
+    }
+
+    public void confirmacion(String titulo, String cuerpo)
+    {
+        AlertDialog.Builder msj = new AlertDialog.Builder(this.contexto);
+        msj.setTitle(titulo);
+        msj.setMessage(cuerpo);
+        msj.setPositiveButton("Listo", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+            }
+        });
         msj.create();
         msj.show();
     }
