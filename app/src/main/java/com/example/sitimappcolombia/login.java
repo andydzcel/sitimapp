41027ec4f29444535e456f959e6ffd9d6520f2f4
andrea camargo <andydzcel@gmail.com>
2021-11-11 01:57:14 -0500

    Mensajes login

--- app/src/main/java/com/example/sitimappcolombia/login.java
+++ app/src/main/java/com/example/sitimappcolombia/login.java
@@ -5,7 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.example.sitimappcolombia.clases.Mensajes;
 
 public class login extends AppCompatActivity {
 
@@ -15,26 +18,37 @@ public class login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Button btnIngresar = (Button) findViewById(R.id.btn_login_ingresar);
-        Button btnRegistrarse = (Button) findViewById(R.id.btn_login_registrarse);
+        Button btnQuieroRegistrarme = (Button) findViewById(R.id.btn_login_registrarse);
+        EditText txtEmail = (EditText) findViewById(R.id.btn_login_email);
+        EditText txtClave = (EditText) findViewById(R.id.btn_login_contrasenia);
 
         btnIngresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(view.getContext(), activity_home_view.class);
-                startActivity(i);
+
+                String email = txtEmail.getText().toString();
+                String clave = txtClave.getText().toString();
+
+                if(email.isEmpty() || clave.isEmpty()) {
+                    new Mensajes(view.getContext()).alerta("Advertencia", "Digite los campos vacíos.");
+                }
+                else
+                {
+                    Intent i = new Intent(view.getContext(), activity_home_view.class);
+                    startActivity(i);
+                }
             }
 
 
         });
 
-        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+        btnQuieroRegistrarme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent in = new Intent(view.getContext(), registroUsuarios.class);
                 startActivity(in);
             }
         });
-
 
 
     }
