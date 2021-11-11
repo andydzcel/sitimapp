89dff8e0806c9de355a25d46ca45d149879570a5
Juan Esteban Ortegon <juanortegon0119@outlook.com>
2021-11-11 01:00:27 -0500

    clase mensajes

--- app/src/main/java/com/example/sitimappcolombia/clases/Mensajes.java
+++ app/src/main/java/com/example/sitimappcolombia/clases/Mensajes.java
@@ -1,2 +1,21 @@
-package com.example.sitimappcolombia.clases;public class Mensajes {
+package com.example.sitimappcolombia.clases;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+public class Mensajes {
+    private Context contexto;
+
+    public Mensajes(Context contexto) {
+        this.contexto = contexto;
+    }
+
+    public void alerta(String titulo, String cuerpo)
+    {
+        AlertDialog.Builder msj = new AlertDialog.Builder(this.contexto);
+        msj.setTitle(titulo);
+        msj.setMessage(cuerpo);
+        msj.create();
+        msj.show();
+    }
 }
