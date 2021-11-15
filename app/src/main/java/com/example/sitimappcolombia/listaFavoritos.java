ce34536969f9f96c20e5514b87196009dc289942
JOHAN-PC\Johan <johan.riincon@gmail.com>
2021-11-14 19:06:55 -0500

    Cambios de id de los elementos, actualización de la vista de lugares favoritos con cardview y recyclerview.

--- app/src/main/java/com/example/sitimappcolombia/listaFavoritos.java
+++ app/src/main/java/com/example/sitimappcolombia/listaFavoritos.java
@@ -2,6 +2,9 @@ package com.example.sitimappcolombia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class listaFavoritos extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class listaFavoritos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_favoritos);
+
+        ImageButton img_button = (ImageButton) findViewById(R.id.imgbtn_itemslistafavoritos_vermas);
+        img_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Creating the instance of PopupMenu
+                PopupMenu popup = new PopupMenu(listaFavoritos.this, img_button);
+                //Inflating the Popup using xml file
+                popup.getMenuInflater().inflate(R.menu.menu_listafavoritos, popup.getMenu());
+
+                popup.show();//showing popup menu
+            }
+        });//closing the setOnClickListener method
     }
 }
